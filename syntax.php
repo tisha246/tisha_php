b05a1f113e37f7e4ed1c42ca6fcac5d5dbf1e1ba
tisha246 <tishathakar54@gmail.com>
2024-01-16 12:28:45 +0530

    changes

---
+++ syntax.php
@@ -0,0 +1,36 @@
+<?php
+
+echo "echo use for print";
+
+
+echo "<h1> HTML code use in php</h1>";
+
+$varname="Tisha";
+
+echo $varname . "<br>";
+
+
+echo "<h1>My name is " . $varname . "</h1>";
+
+
+$a=10;
+$b=20;
+$sum=$a+$b;
+
+echo "<h1> Sum of a & b are : ".$sum ."</h1>";
+
+// Single line comment
+
+
+/*
+multiple
+line
+comment
+*/
+
+?>
+
+
+<!--
+HTML COMMENT
+-->
